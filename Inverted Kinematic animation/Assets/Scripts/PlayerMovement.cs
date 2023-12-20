209391b216f218ffc31adaec5d459283d8293ae7
Otterly-Cute <144897039+Otterly-Cute@users.noreply.github.com>
2023-12-20 16:57:31 +0100

    everything is messed up and i want to cry

--- Inverted Kinematic animation/Assets/Scripts/PlayerMovement.cs
+++ Inverted Kinematic animation/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 movement;
+    public Rigidbody2D myBody;
+    public Animator myAnimator;
+    public bool facingRight = true;
+
+    [SerializeField] private float speed = 1.4f;
+
+    public void Awake()
     {
+        myBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+    public void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            myAnimator.SetFloat("x", movement.x);
+           // this.transform.Translate(new Vector3(movement.x,0), Space.World);
+            myAnimator.SetFloat("y", movement.y);
+
+            myAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isWalking", false);
+        }
         
+        float h = Input.GetAxis("Horizontal");
+        if (h > 0 && !facingRight)
+        { Flip(); }
+        else if (h < 0 && facingRight)
+        { Flip(); }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Flip()
     {
-        
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void FixedUpdate()
+    {
+        myBody.velocity = movement * speed;
+
+       /* bool flipped=movement.x < 0;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f)); */
     }
 }
